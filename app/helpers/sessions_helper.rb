a0f34c04762f2d07d37164eb2e397518ad80418b
Richard Taylor <moomerman@gmail.com>
2011-07-15 11:34:30 +0100

    using the correct google charts api url

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
   def google_authenticator_qrcode(user)
     data = "otpauth://totp/two_factor_demo?secret=#{user.auth_secret}"
     data = Rack::Utils.escape(data)
-    url = "https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
+    url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=#{data}"
     return image_tag(url, :alt => 'Google Authenticator QRCode')
   end
 end
